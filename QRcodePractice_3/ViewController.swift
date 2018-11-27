784e3f6e4fc515bd16501bdced219bad2f9c4548
Mac <macong19940421.gmail.com>
2018-11-27 13:44:38 +0800

    Initial Commit

---
+++ QRcodePractice_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRcodePractice_3
+//
+//  Created by 马胖 on 27/11/18.
+//  Copyright © 2018 macong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
