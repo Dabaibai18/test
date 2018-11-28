6ea3d6d1b0f9193d7e6d88d53dc3862c29a42203
Mac <macong19940421.gmail.com>
2018-11-28 11:14:06 +0800

    scrollerview can scroll but height is bad

---
+++ QRcodePractice_3/LearnMoreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LearnMoreViewController.swift
+//  QRcodePractice_3
+//
+//  Created by 马胖 on 28/11/18.
+//  Copyright © 2018 macong. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+
+class LearnMoreViewController: UIViewController {
+
+    @IBOutlet weak var contentscrollview: UIScrollView!
+    @IBOutlet weak var content_view: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        //have problem in the height
+        content_view.frame.size.height = view.frame.size.height * 2
+        
+        contentscrollview.contentSize = content_view.frame.size
+    }
+    
+
+    
+
+}
